35caf06858d40b2c4d64b895aa595b5e6d78f7b1
shahadat4536 <shahadathossain4536@gmail.com>
2022-04-28 17:28:12 +0600

    token genarate and send client site

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
-
+//require('crypto').randomBytes(256).toString('base64')
 ////testapp
 //I2kDNS7vcI6EFU6g
 
@@ -21,6 +22,12 @@ async function run() {
   try {
     await client.connect();
     const productCollection = client.db("test").collection("devices");
+
+    app.post("/login", (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET);
+      res.send({ token });
+    });
 
     app.post("/product", async (req, res) => {
       const product = req.body;
